83a59f339a26edf8beb5c285b2648ee8a3b31cb7
David Giroux <DavidGiroux149@gmail.com>
2023-12-11 17:11:28 -0500

    add constructor (method initialize) to Player class

--- player.rb
+++ player.rb
@@ -1,2 +1,12 @@
 class Player 
   
+
+
+  # Method to initialize new object instance
+  # Includes players names and their starting lives
+  def initialize(name)
+    @name = name
+    @lives = 3
+  end 
+
+  
