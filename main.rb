cb23a2eed22e37e95801d38ec03ce885d7371ddf
David Giroux <DavidGiroux149@gmail.com>
2023-12-11 21:51:35 -0500

    create new player and game object instantiations

--- main.rb
+++ main.rb
@@ -2,3 +2,8 @@ require_relative 'player'
 require_relative 'game'
 require_relative 'question'
 
+player1 = Player.new("Player 1")
+player2 = Player.new("Player 2")
+
+game = Game.new(player1, player2)
+game.start
