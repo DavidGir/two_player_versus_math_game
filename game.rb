e39afdd064e0edab992a7e6927b99260f2ccb9c4
David Giroux <DavidGiroux149@gmail.com>
2023-12-11 19:11:13 -0500

    add method switch_player to change current player

--- game.rb
+++ game.rb
@@ -8,4 +8,10 @@ class Game
     @current_player = @player1
   end
 
+  def switch_player 
+    # Current player will be checked by ternary; if it is player1 return to player2 if false return to player1
+    @current_player = @current_player == @player1 ? @player2 : @player1
+  end
+
+ 
 
