27588c7d33a4034d6f19f7503078cf594362f878
David Giroux <DavidGiroux149@gmail.com>
2023-12-11 20:33:54 -0500

    add method override to_s and method that checks answer

--- question.rb
+++ question.rb
@@ -1,4 +1,3 @@
-
 class Question 
 
   # Initialize object instance with two random numbers between 1 to 20 for simple addition questions
@@ -7,3 +6,17 @@ class Question
     @number2 = rand(1..20)
     @answer = @number1 + @number2
   end
+
+  # Generate the math questions
+  def to_s
+    "What does #{@number1} plus #{@number2} equal?"
+  end 
+
+  # Method to chck the answer; player_answer is the input from user/player received from get.chomps in Game class
+  def correct_answer?(player_answer)
+    player_answer == @answer
+  end 
+end 
+
+# question = Question.new
+# puts question
